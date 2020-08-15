91ed7eda7690db1b20ad6a8ad623bf2a9bf0ac31
premprakash7 <prem.darini@hotmail.com>
2020-08-15 11:28:58 +0530

    feat: typo in code

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -3448,29 +3448,29 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 async function run() {
-
     try {
-        const token = core.getInput('token');
-        const title = core.getInput('title');
-        const body = core.getInput('body');
-        const assignees = core.getInput('assignees');
+        const token = core.getInput("token");
+        const title = core.getInput("title");
+        const body = core.getInput("body");
+        const assignees = core.getInput("assignees");
 
-        const octokit = new github.Github(token);
+        const octokit = new github.GitHub(token);
 
         const response = await octokit.issues.create({
             ...github.context.repo,
             title,
             body,
-            assignees: assignees ? assignees.split("\n") : undefined
+            assignees: assignees ? assignees.split("\n") : undefined,
         });
 
-        core.setOutput("issue", JSON.stringify(response.data))
+        core.setOutput("issue", JSON.stringify(response.data));
     } catch (error) {
         core.setFailed(error.message);
     }
 }
 
 run();
+
 
 /***/ }),
 
--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -2,23 +2,22 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function run() {
-
     try {
-        const token = core.getInput('token');
-        const title = core.getInput('title');
-        const body = core.getInput('body');
-        const assignees = core.getInput('assignees');
+        const token = core.getInput("token");
+        const title = core.getInput("title");
+        const body = core.getInput("body");
+        const assignees = core.getInput("assignees");
 
-        const octokit = new github.Github(token);
+        const octokit = new github.GitHub(token);
 
         const response = await octokit.issues.create({
             ...github.context.repo,
             title,
             body,
-            assignees: assignees ? assignees.split("\n") : undefined
+            assignees: assignees ? assignees.split("\n") : undefined,
         });
 
-        core.setOutput("issue", JSON.stringify(response.data))
+        core.setOutput("issue", JSON.stringify(response.data));
     } catch (error) {
         core.setFailed(error.message);
     }
